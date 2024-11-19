ed81f972e3012df768504e0747e3284b09fc8c3e
AfirstKussoy <s22210187@student.unklab.ac.id>
2024-11-19 17:05:38 +0800

    adding firebase component home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,13 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 const Home = () => {
+  const [home, setHome] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const homeRef = ref(db, "home");
+    onValue(homeRef, (snapshot) => {
+      const data = snapshot.val();
+      setHome(data);
+    });
+  }, []);
   return (
     <div className="section" id="home" data-stellar-background-ratio="0.5">
       <div className="container">
         <div className="disply-table">
           <div className="table-cell" data-aos="fade-up" data-aos-delay={0}>
-            <h4>AFIRST LAWRENCE KUSSOY</h4>
-            <h1>
-              I'M A STUDENT OF COMPUTER SCIENCE
-            </h1>
+            <h4>{home.home1}</h4>
+            <h1>{home.home2}</h1>
           </div>
         </div>
       </div>
