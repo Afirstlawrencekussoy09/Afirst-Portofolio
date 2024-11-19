69214f4b28d4e1c3fd299152e3eb3891c4092ac6
AfirstKussoy <s22210187@student.unklab.ac.id>
2024-11-19 17:01:00 +0800

    adding firebase component header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,19 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 const Header = () => {
+  const [header, setHeader] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const headerRef = ref(db, "header");
+    onValue(headerRef, (snapshot) => {
+      const data = snapshot.val();
+      setHeader(data);
+    });
+  }, []);
   return (
     <header className="navbar-fixed-top">
       <nav>
         <ul>
           <li>
-            <a href="#about">About</a>
+            <a href="#about">{header.header1}</a>
           </li>
           <li>
-            <a href="#experience">Education</a>
+            <a href="#education">{header.header2}</a>
           </li>
           <li>
-            <a href="#projects">projects</a>
+            <a href="#projects">{header.header}</a>
           </li>
           <li>
-            <a href="#contact">contact</a>
+            <a href="#contact">{header.header3}</a>
           </li>
         </ul>
       </nav>
